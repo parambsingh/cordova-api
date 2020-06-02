b41cee3a77e40633d94d52d4860681420bf508a5
root <parambsingh>
2020-06-02 10:39:32 +0530

    Initial Project files

---
+++ contact.php
@@ -0,0 +1,7 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Content-Type: application/json');
+echo json_encode(['status'=>'Contact Saved']);
---
+++ index.php
@@ -0,0 +1,1 @@
+<h3>Cordova APIs</h3>
